585cb3070b6cc10ad11bf04759c6fb577a539393
yangzhipeng <yangzhipeng@outlook.com>
2019-12-21 21:38:02 +0800

    PC-登录模块-数据绑定及校验(上，中)

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,21 +6,21 @@
       <div class="title">
       <img src="../../assets/img/logo_index.png" alt="">
     </div>
-    <!-- 放置表单 -->
-    <el-form>
-      <!-- 表单里面  放置 input/select/checkbox  相当于一行 -->
-      <el-form-item>
-        <el-input placeholder="请输入手机号">
+    <!-- 放置表单 el-form model 绑定数据对象-->
+    <el-form :model="loginForm" :rules="loginRules">
+      <!-- 表单里面  里面 prop要写要检验的字段名  放置 input/select/checkbox  相当于一行 -->
+      <el-form-item prop="mobile">
+        <el-input v-model="loginForm.mobile" placeholder="请输入手机号">
         </el-input>
       </el-form-item>
       <!-- 表单域 -->
-      <el-form-item>
-        <el-input style="width:60%" placeholder="验证码"></el-input>
+      <el-form-item prop="code">
+        <el-input v-model="loginForm.code" style="width:65%" placeholder="验证码"></el-input>
         <el-button plain style="float : right">验证码</el-button>
       </el-form-item>
-      <el-form-item>
+      <el-form-item  prop="check">
         <!--复选框  -->
-       <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+       <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" style="width:100%">登录</el-button>
@@ -32,7 +32,37 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否勾选，同意被坑
+      },
+      loginRules: {
+        // 验证规则对象 key (字段名)： value(规则 => [])
+        mobile: [{ required: true, message: '请输入你的手机号' }, {
+          pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确'
+        }],
+        code: [{ required: true, message: '请输入你的验证码' }, {
+          pattern: /^\d{6}$/, message: '验证码不正确'
+        }],
+        check: [{ validator: function (rule, value, callback) {
+          // 自定义校验函数
+          // rule 规则 没啥用
+          // value 要校验的字段的值
+          // callback 是一个回调函数
+          if (value) {
+            // 认为已经勾选
+            callback() // 认为当前的规则校验通过了
+          } else {
+            // 认为没有勾选
+            callback(new Error('你应该同意我们的霸王条款，无条件的接受我们欺负你')) // 如果没有勾选  认为当前校验失败  应该停止
+          }
+        } }]
+      }
+    }
+  }
 }
 </script>
 
