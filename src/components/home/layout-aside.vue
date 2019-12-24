1b1cccc24a1af85e677b324441a8b8c2387ee776
yangzhipeng <yangzhipeng@outlook.com>
2019-12-24 21:26:32 +0800

    PC-主页模块-左侧导航菜单(下)

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="layout-aside">
+    <div class="title">
+      <img src="../../assets/img/logo_admin.png" alt="">
+    </div>
     <!-- 左侧导航组件 -->
-    <el-menu>
+    <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
       <!-- 没有折叠选项 -->
       <el-menu-item>首页</el-menu-item>
       <!-- 二级折叠菜单 -->
@@ -9,10 +12,22 @@
         <template slot="title">
           <span>内容管理</span>
         </template>
+        <!-- 匿名插槽 -->
         <el-menu-item>发布文章</el-menu-item>
         <el-menu-item>文章列表</el-menu-item>
         <el-menu-item>评论管理</el-menu-item>
         <el-menu-item>素材管理</el-menu-item>
+
+      </el-submenu>
+       <el-submenu>
+        <template slot="title">
+          <span>粉丝管理</span>
+        </template>
+        <!-- 匿名插槽 -->
+        <el-menu-item>图文数据</el-menu-item>
+        <el-menu-item>粉丝概况</el-menu-item>
+        <el-menu-item>粉丝画像</el-menu-item>
+        <el-menu-item>粉丝列表</el-menu-item>
 
       </el-submenu>
       <el-menu-item>账户信息</el-menu-item>
@@ -26,6 +41,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.layout-aside{
+  width: 230px;
+  .el-menu{
+border-right: none;
+  };
+  .title{
+    background-color: #2e2f32;
+    text-align: center;
+    padding: 10px 0;
+    img{
+      height: 35px;
+    }
+  }
+}
 </style>
