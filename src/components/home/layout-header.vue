f837f95bb4b41dfd35f048a04b6fbd363ecdd2c0
yangzhipeng <yangzhipeng@outlook.com>
2019-12-25 10:25:27 +0800

    PC-主页模块-头部结构及下拉菜单

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,55 @@
+<template>
+  <!-- el- row -->
+  <el-row class="layout-header" type="flex" align="middle">
+      <!-- 先定义一行 -->
+      <el-col class="left" :span="12">
+          <i class="el-icon-s-fold">
+              <span>江苏传智播客教育科技股份有限公司</span>
+          </i>
+      </el-col>
+      <el-col class="right" :span="12">
+          <el-row type="flex" justify="end" align="middle">
+              <img src="../../assets/img/tou.jpeg" alt="">
+              <!-- 下拉菜单 -->
+              <el-dropdown>
+                  <span>愚者</span>
+                  <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>个人信息</el-dropdown-item>
+                      <el-dropdown-item>git地址</el-dropdown-item>
+                      <el-dropdown-item>退出</el-dropdown-item>
+
+                  </el-dropdown-menu>
+              </el-dropdown>
+          </el-row>
+      </el-col>
+
+  </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+ .layout-header{
+     height: 60px;
+     .left{
+         font-size: 20px;
+         span{
+         color: #2c3e50;
+         font-size: 16px;
+         margin-left: 4px;
+         }
+     }
+     .right{
+          img{
+              width: 40px;
+              height: 40px;
+              border-radius: 50%;
+              margin-right: 5px;
+          }
+     }
+ }
+</style>
